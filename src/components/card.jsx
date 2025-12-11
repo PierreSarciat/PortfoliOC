bcba8bdb406ad72b5168b45f92aa6047940f07f3
PierreSarciat <phsarciat@mailo.com>
2025-12-11 22:53:11 +0100

    composant card

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,6 +3,8 @@ import './card.scss';
 import { Link } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
 import ButtonT from "./button-tag.jsx";
+import logoGit from "@logo/logoGitHub.png";
+import Demo from "@logo/demo.png";
 
 const Card = ({ img, title, link, alt, description, tag_text }) => {
     const navigate = useNavigate();
@@ -12,32 +14,36 @@ const Card = ({ img, title, link, alt, description, tag_text }) => {
         window.scrollTo(0, 0);
     };
     return (
-        <a href={link} onClick={handleClick} className="card-link">
-            <div className="card">
-                {/* Conteneur pour l'image et l'overlay */}
-                <div className="card__image-container">
-                    <img src={img} alt={alt} className="card__image" />
-                    {/* Titre superposé à l'image (toujours visible) */}
-                    <h3 className="card__title">{title}</h3>
-                    {/* Overlay vide (apparaît au survol) */}
-                    <div className="card__overlay"></div>
-                </div>
-                {/* Description en dehors de l'overlay */}
-                <p className="card__description">{description}</p>
-                {/* Affichage des tags */}
-                <div className="buttonTag">
-                    {Array.isArray(tag_text) ? (
-                        tag_text.map((tag, index) => (
-                            <ButtonT key={index} tag_text={tag} />
-                        ))
-                    ) : (
 
-                        <ButtonT tag_text={tag_text} />
+        <div className="card">
+            {/* Conteneur pour l'image et l'overlay */}
+            <div className="card__image-container">
+                <img src={img} alt={alt} className="card__image" />
+                {/* Titre superposé à l'image (toujours visible) */}
+                <h3 className="card__title">{title}</h3>
+                {/* Overlay vide (apparaît au survol) */}
+                <div className="card__overlay"></div>
 
-                    )}
-                </div>
             </div>
-        </a>
+            {/* Description en dehors de l'overlay */}
+            <p className="card__description">{description}</p>
+            {/* Affichage des tags */}
+            <div className="buttonTag">
+                {Array.isArray(tag_text) ? (
+                    tag_text.map((tag, index) => (
+                        <ButtonT key={index} tag_text={tag} />
+                    ))
+                ) : (
+
+                    <ButtonT tag_text={tag_text} />
+
+                )}
+            </div>
+            <div className='logoCard'>
+                <a href="https://github.com/PierreSarciat/" target="_blank" rel="noopener noreferrer" ><img src={logoGit} alt="GitHub" />Code</a>
+                <a href="" target="_blank" rel="noopener noreferrer" ><img src={Demo} alt="demo" />Démo</a>
+            </div>
+        </div>
     );
 };
 export default Card;
