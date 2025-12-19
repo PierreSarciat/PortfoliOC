3d618735bf3ecf7a369a395d1a593ac136fda00a
PierreSarciat <phsarciat@mailo.com>
2025-12-19 19:24:44 +0100

    animation skills

--- src/components/skillsContent.jsx
+++ src/components/skillsContent.jsx
@@ -14,7 +14,7 @@ const SkillsContent = ({ skills }) => {
                     observer.disconnect(); // une seule fois
                 }
             },
-            { threshold: 0.3 }
+            { threshold: 0.4 }
         );
 
         if (sectionRef.current) observer.observe(sectionRef.current);
