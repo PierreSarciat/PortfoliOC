eec7fd67839a80c220a9642aaf38ae07dab0387d
PierreSarciat <phsarciat@mailo.com>
2025-12-20 17:36:04 +0100

    suppressio routes projets

--- src/router.jsx
+++ src/router.jsx
@@ -2,12 +2,6 @@ import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx';
 import Home from '@pages/home.jsx';
-import Carducci from '@pages/carducci.jsx';
-import Grimoire from '@pages/grimoire.jsx';
-import Kasa from '@pages/kasa.jsx';
-import Bluel from '@pages/bluel.jsx';
-import Maker from '@pages/maker.jsx';
-import Booki from '@pages/booki.jsx';
 
 
 const router = createBrowserRouter([
@@ -15,13 +9,8 @@ const router = createBrowserRouter([
     path: '/',
     element: <App />,
     children: [
-      { index: true, element: <Home /> },
-      { path: 'carducci', element: <Carducci /> },
-      { path: 'grimoire', element: <Grimoire /> },
-      { path: 'kasa', element: <Kasa /> },
-      { path: 'maker', element: <Maker /> },
-      { path: 'bluel', element: <Bluel /> },
-      { path: 'booki', element: <Booki /> },
+      { index: true, element: <Home /> }
+
     ]
   },
 ],
@@ -33,4 +22,3 @@ const router = createBrowserRouter([
 export default function Router() {
   return <RouterProvider router={router} />;
 }
-
