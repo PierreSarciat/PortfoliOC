0056cc83850b297c4638719b97980aa1428867d2
PierreSarciat <phsarciat@mailo.com>
2026-01-14 11:01:18 +0100

    maj card

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,49 +1,38 @@
 import React from "react";
-import './card.scss';
-import { Link } from 'react-router-dom';
-import { useNavigate } from "react-router-dom";
+import "./card.scss";
 import ButtonT from "./button-tag.jsx";
 import logoGit from "@logo/logoGitHub.png";
 import Demo from "@logo/demo.png";
 
 const Card = ({ img, title, alt, description, tag_text, linkGithub, linkDemo }) => {
-    const navigate = useNavigate();
-    const handleClick = (e) => {
-        e.preventDefault();
-        navigate(link);
-        window.scrollTo(0, 0);
-    };
     return (
-
         <div className="card">
-            {/* Conteneur pour l'image et l'overlay */}
-            <div className="card__image-container">
-                <img src={img} alt={alt} className="card__image" />
-                {/* Titre superposé à l'image (toujours visible) */}
-                <h3 className="card__title">{title}</h3>
-                {/* Overlay vide (apparaît au survol) */}
-                <div className="card__overlay"></div>
+            <h3 className="card__title">{title}</h3>
 
+            <div className="card__image-container">
+                <p className="card__description">{description}</p>
+                <img src={img} alt={alt} className="card__image" />
+                <div className="card__overlay"></div>
             </div>
-            {/* Description en dehors de l'overlay */}
-            <p className="card__description">{description}</p>
-            {/* Affichage des tags */}
+
             <div className="buttonTag">
-                {Array.isArray(tag_text) ? (
-                    tag_text.map((tag, index) => (
+                {Array.isArray(tag_text)
+                    ? tag_text.map((tag, index) => (
                         <ButtonT key={index} tag_text={tag} />
                     ))
-                ) : (
-
-                    <ButtonT tag_text={tag_text} />
-
-                )}
+                    : <ButtonT tag_text={tag_text} />}
             </div>
-            <div className='logoCard'>
-                <a href={linkGithub} target="_blank" rel="noopener noreferrer" ><img src={logoGit} alt="GitHub" />Code</a>
-                <a href={linkDemo} target="_blank" rel="noopener noreferrer" ><img src={Demo} alt="demo" />Démo</a>
+
+            <div className="logoCard">
+                <a href={linkGithub} target="_blank" rel="noopener noreferrer">
+                    <img src={logoGit} alt="GitHub" /> Code
+                </a>
+                <a href={linkDemo} target="_blank" rel="noopener noreferrer">
+                    <img src={Demo} alt="Démo" /> Démo
+                </a>
             </div>
         </div>
     );
 };
+
 export default Card;
