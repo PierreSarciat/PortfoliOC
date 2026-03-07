afb0d3f14d954fb7ac0db296156d1a1c6394a6d6
PierreSarciat <phsarciat@mailo.com>
2026-03-07 21:03:17 +0100

    changement structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,23 @@ import { Outlet } from "react-router-dom";
 import Header from '@components/header/header.jsx';
 import Footer from '@components/footer/footer.jsx';
 import ScrollToTop from '@components/ScrollToTop/scrollToTop.jsx';
+import Hero from '@sections/hero/hero.jsx';
+import SkillsContent from '@sections/skillsContent/skillsContent.jsx';
+import AboutMe from '@sections/aboutMe/aboutMe.jsx';
+import ProjectsSection from '@projectsSection/projectsSection.jsx';
+
 
 
 function App() {
   return (
     <>
       <ScrollToTop />
-      hero
+      <Hero />
+      <SkillsContent />
+      <AboutMe />
+      <ProjectsSection />
+
+
     </>
   );
 }
