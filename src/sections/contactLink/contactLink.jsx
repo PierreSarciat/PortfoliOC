1b78da3f299fed36b5d748b1d2bf1b97195ad48a
PierreSarciat <phsarciat@mailo.com>
2026-03-11 19:15:41 +0100

    ajout section contact

---
+++ src/sections/contactLink/contactLink.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Button from "@components/button/button.jsx";
+import { Link } from 'react-router-dom';
+import { useInView } from '@hooks/useInView';
+import './ContactLink.scss';
+
+const ContactLink = () => {
+    const [ref, isVisible] = useInView({ threshold: 0.3 });
+    return (
+
+        <div className='ContactLink__background' id="contact">
+            <div
+                ref={ref}
+                className={`ContactLink__content ${isVisible ? 'is-visible' : ''}`}
+            >
+                <h1>Vous préparer seul est possible.
+                    Être accompagné change tout.</h1>
+                <p>Discutons ensemble de votre projet et de vos objectifs lors d'un premier entretien gratuit.</p>
+                <Link to="/contact">
+                    <Button text="Contactez-moi" />
+                </Link>
+            </div>
+        </div>
+
+
+    );
+};
+
+export default ContactLink;
