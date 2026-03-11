0f413690f25b9a8f10959845ced22b776515200c
PierreSarciat <phsarciat@mailo.com>
2026-03-11 20:09:58 +0100

    ajout page contact

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -15,51 +15,51 @@ const Contact = ({ contactEmail, contactPhone, contactLocalisation }) => {
 
     return (
         <div className='contact__background'>
-            <section className='section'>
-                <div className='contact__container'>
+
+            <div className='contact__container'>
 
 
-                    <div
-                        ref={contentRef}
-                        className={`contact__content ${isVisible ? 'is-visible' : ''}`}
-                    >
-                        <div className="information">
-                            <h3>Informations de contact</h3>
-                            <div className='info'>
-                                <i className="fa-solid fa-envelope"></i>
-                                <p><span className='item'>E-mail</span><br />{contactEmail}</p>
-                            </div>
-                            <div className='info'>
-                                <i className="fa-solid fa-phone"></i>
-                                <p><span className='item'>Téléphone</span><br />{contactPhone}</p>
-                            </div>
-                            <div className='info'>
-                                <i className="fa-solid fa-location-dot"></i>
-                                <p><span className='item'>Localisation</span><br />{contactLocalisation}</p>
-                            </div>
+                <div
+                    ref={contentRef}
+                    className={`contact__content ${isVisible ? 'is-visible' : ''}`}
+                >
+                    <div className="information">
+                        <h3>Informations de contact</h3>
+                        <div className='info'>
+                            <i className="fa-solid fa-envelope"></i>
+                            <p><span className='item'>E-mail</span><br />{contactEmail}</p>
                         </div>
-
-                        <div className="contact__form">
-                            <ContactForm />
+                        <div className='info'>
+                            <i className="fa-solid fa-phone"></i>
+                            <p><span className='item'>Téléphone</span><br />{contactPhone}</p>
+                        </div>
+                        <div className='info'>
+                            <i className="fa-solid fa-location-dot"></i>
+                            <p><span className='item'>Localisation</span><br />{contactLocalisation}</p>
                         </div>
                     </div>
 
-
-
-
-                    <div
-                        ref={contactRef}
-                        className={`contact ${isVisible ? 'is-visible' : ''}`}
-                    >
-                        <h2>Contactez-moi</h2>
-                        <p>
-                            Vous avez une question ou souhaitez en savoir plus sur mes programmes ?
-                            N'hésitez pas à me contacter.
-                        </p>
+                    <div className="contact__form">
+                        <ContactForm />
                     </div>
-
                 </div>
-            </section>
+
+
+
+
+                <div
+                    ref={contactRef}
+                    className={`contact ${isVisible ? 'is-visible' : ''}`}
+                >
+                    <h2>Contactez-moi</h2>
+                    <p>
+                        Vous avez une question ou souhaitez en savoir plus sur mes programmes ?
+                        N'hésitez pas à me contacter.
+                    </p>
+                </div>
+
+            </div>
+
         </div>
     );
 };
