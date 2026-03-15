8f7b146e3af4ef2600142f3558daabc748434766
PierreSarciat <phsarciat@mailo.com>
2026-03-15 09:25:12 +0100

    changement slogan

--- src/layout.jsx
+++ src/layout.jsx
@@ -1,9 +1,16 @@
 import Navbar from "@components/navbar/navbar.jsx";
 import Footer from "@components/footer/footer.jsx";
 import ScrollToTop from '@components/ScrollToTop/scrollToTop.jsx';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { Outlet } from "react-router-dom";
 
 function Layout() {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
     return (
         <>
             <ScrollToTop />
