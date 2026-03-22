ca9e879b38055f75aef5317f977b596af14a5c8c
PierreSarciat <phsarciat@mailo.com>
2026-03-22 12:19:24 +0100

    plugin image optimizer

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-import { fileURLToPath } from 'url'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react()],
-   base: '/PortfoliOC/', 
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      png: { quality: 80 },
+      jpeg: { quality: 80 },
+      jpg: { quality: 80 },
+      webp: { quality: 80 },
+      avif: { quality: 50 },
+    }),
+  ],
+  base: '/PortfoliOC/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
       '@components': path.resolve(__dirname, './src/components'),
       '@sections': path.resolve(__dirname, './src/sections'),
       '@styles': path.resolve(__dirname, './src/styles'),
-       '@images': path.resolve(__dirname, './public/images'),
-       '@assets': path.resolve(__dirname, './src/assets'),
-        '@logo': path.resolve(__dirname, './src/assets/logo'),
-        '@hooks':fileURLToPath(new URL('./src/hooks', import.meta.url)), 
-        '@pages':path.resolve(__dirname, './src/pages')
+      '@images': path.resolve(__dirname, './public/images'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@logo': path.resolve(__dirname, './src/assets/logo'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@pages': path.resolve(__dirname, './src/pages'),
     },
   },
- 
-})
+});
